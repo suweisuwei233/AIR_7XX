85dc427a07d830a07b9d5cbdb5a72d8b11f20675
suweisuwei233 <2632319495@qq.com>
2024-05-02 02:27:37 +0800

    12312312

---
+++ 核心固件/core_V1106/my_demo/01_demo/main.lua
@@ -0,0 +1,54 @@
+
+PROJECT = "hello_world"
+VERSION = "1.0.0"
+
+sys = require("sys")
+print(_VERSION)
+
+sys.timerLoopStart(function()
+    print("Test Latus\r\n")
+end, 1000)
+
+local uartid = 1 -- 根据实际设备选取不同的uartid
+-- 初始化
+local result = uart.setup(uartid, -- 串口id
+115200, -- 波特率
+8, -- 数据位
+1 -- 停止位
+)
+-- sys.taskInit(function()
+--     while 0 do
+--         sys.wait(50)
+--     end
+-- end)
+-- sys.taskInit(function()
+--     while 0 do
+--         uart.write(uartid,
+--             "你好这个是一个测试你好这个是一个测试你好这个是一个测试你好这个是一个测试\r\n");
+--         sys.wait(50)
+--     end
+-- end)
+-- sys.timerLoopStart(uart.write, 10000, uartid,
+--     "你好这个是一个测试你好这个是一个测试你好这个是一个测试你好这个是一个测试\r\n")
+
+usart1_printf= function(s, ...)
+    return uart.write(uartid,s:format(...))
+end
+usart1_printf("%s\n", "Hello World!")
+
+sys.taskInit(function()
+    local str = ""
+    while 1 do
+        str = uart.read(uartid,0x800)
+        sys.wait(1000);
+        uart.write(uartid,str);
+        usart1_printf("the get string len : %d\r\n",string.len(str));
+            log.info("GET str","str:"..str);
+            log.error("GET str","str:"..str);
+        sys.wait(1000);
+
+    end
+end)
+
+
+sys.run()
---
+++ 核心固件/core_V1106/readme.txt
@@ -0,0 +1,5 @@
+固件说明: https://wiki.luatos.com/chips/air780e/firmware.html
+文档1: https://wiki.luatos.com
+文档2: http://780eg.cn
+QQ群6: 387396364
+QQ群22: 627625461
